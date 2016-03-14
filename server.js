eb0a63740d43c9093236a6ee2115f52eabbdd8e4
albertorevel <alberto.revel@gmail.com>
2016-03-14 12:31:59 +0100

    Server working (cannot use https)

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 var express = require('express');
-var keys = require('./serverKeys.json');
+var jsonfile = require('jsonfile');
+var util = require('util');
 var requestify = require('requestify');
+var requests = require('request');
 var app = express();
 var authKey = "";
 var clientToken = "";
+var file = './serverKeys.json';
+var keys;
 
 app.get('/notify/:id', function (req, res) {
    var reqId = req.params.id;
    console.log("New request to id: %s", reqId);
+   console.log(authKey);
+   console.log(clientToken);
       
-   requestify.request('https://gcm-http.googleapis.com/gcm/send', {
+  /* requestify.request('http://gcm-http.googleapis.com/gcm/send', {
     method: 'POST',
     body: {
+		'data' : {
         'id': '0ababa34',
 		'nombre': 'Alarma 1',
 		'descripcion': 'Esta alarma ha sido lanzada',
@@ -27,19 +34,47 @@ app.get('/notify/:id', function (req, res) {
     headers: {
         'Content-Type': 'application/json',
 		'Authorization': 'key='+authKey
-    })
+    }
   
 }).then(function(response) {
+	console.log(response.getCode);
 	if(response.getCode() == 200) {
 		res.send('Notification sent');
 	};
-})
+}) */
    
+  
+   requests.post({ 
+	headers: {
+		'Content-Type': 'application/json',
+		'Authorization': 'key='+authKey
+	   },
+	url: 'http://gcm-http.googleapis.com/gcm/send',
+	 json: {
+		'data' : {
+        'id': '0ababa34',
+		'nombre': 'Alarma 1',
+		'descripcion': 'Esta alarma ha sido lanzada',
+		'imagen': 'null',
+		'origen': 'tirador baño',
+		'relativo': 'Miquel Roig',
+		'zona': 'Hab. 102',
+		'estado': '0'
+		},
+		'to': clientToken
+        
+    }, function(error, response, body) {
+		console.log(body);
+	}
+   });
    
 })
 
 app.post('/register',function(req, res) {
 	var reqId = req.body.id;
+	var reqToken = req.body.token;
+	console.log("requestID: %s", reqId);
+	console.log("request token: %s", reqToken);
 	res.sendStatus(404);
 })
 
@@ -52,6 +87,10 @@ var server = app.listen(8081, function () {
   var port = server.address().port
 
   console.log("Example app listening at http://%s:%s", host, port)
+  
+  
+
+  keys = jsonfile.readFileSync(file);
   
   authKey = keys.authKey;
   clientToken = keys.clientToken;
